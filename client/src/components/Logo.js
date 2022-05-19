a222e4706480d48a34e3c3ecb6f3f4a64b95e495
AlfonzoVillarreal <alfonzovillarreal2@yahoo.com>
2022-05-19 17:55:39 -0500

    files so far

---
+++ client/src/components/Logo.js
@@ -0,0 +1,7 @@
+import logo from '../assets/images/logo.svg'
+
+const Logo = () => {
+    return <img src={logo} alt='job_hub' className='logo' />
+}
+
+export default Logo
---
+++ client/src/pages/Landing.js
@@ -0,0 +1,33 @@
+
+import main from '../assets/images/main.svg'
+import Wrapper from '../assets/wrappers/LandingPage'
+import { Logo } from '../components'
+import { Link } from 'react-router-dom'
+
+const Landing = () => {
+    return (
+        <Wrapper>
+            <nav>
+                <Logo />
+            </nav>
+            <div className="container page">
+                <div className="info">
+                    <h1>
+                        job <span>tracking</span> app
+                    </h1>
+                    <p>
+                    I'm baby four dollar toast tote bag master cleanse hammock. Tousled salvia plaid, tofu lumbersexual activated charcoal slow-carb dreamcatcher vape YOLO lyft heirloom man bun banh mi. Crucifix cornhole aesthetic 90's.
+                    </p>
+                    <Link to="/register" className="btn btn-hero">
+                        Login/Register
+                    </Link>
+                </div>
+                <img src={main} alt="job hunt" className="img main-img"></img>
+            </div>
+        </Wrapper>
+    )
+}
+
+
+
+export default Landing
