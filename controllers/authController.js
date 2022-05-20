99eafa27f8a0b22db33830a3067f92d59dee3d90
AlfonzoVillarreal <alfonzovillarreal2@yahoo.com>
2022-05-19 22:50:35 -0500

    5-19-22

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,32 @@
 import User from '../models/User.js'
 import { StatusCodes } from 'http-status-codes'
+import { BadRequestError } from '../errors/index.js'
+
+
 
 const register = async (req, res) => {
-    const user = await User.create(req.body)
-    res.status(StatusCodes.CREATED).json({ user })
+    const {name,email,password} = req.body
+
+    if (!name || !email || !password){
+        throw new BadRequestError('please provide all values')
+    }
+    const UserAlreadyExists = await User.findOne({email});
+    if (UserAlreadyExists){
+        throw new BadRequestError('Email already in use')
+    }
+    const user = await User.create({ name, email, password})
+    const token = user.createJWT()
+    res
+        .status(StatusCodes.OK)
+        .json({ 
+            user:{
+                email:user.email, 
+                lastName:user.lastName, 
+                location:user.location, 
+                name: user.name
+            }, 
+        token, 
+        loction: user.location })
 }
 
 const login = async (req, res) => {
