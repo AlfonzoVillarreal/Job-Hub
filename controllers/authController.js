ac3a04b86d9e98ff2e7a1e6dad886de4f0379ea8
AlfonzoVillarreal <alfonzovillarreal2@yahoo.com>
2022-05-20 12:03:50 -0500

    5-20-22 catching up

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,6 @@
 import User from '../models/User.js'
 import { StatusCodes } from 'http-status-codes'
-import { BadRequestError } from '../errors/index.js'
+import { BadRequestError, UnauthenticatedError } from '../errors/index.js'
 
 
 
@@ -10,27 +10,42 @@ const register = async (req, res) => {
     if (!name || !email || !password){
         throw new BadRequestError('please provide all values')
     }
-    const UserAlreadyExists = await User.findOne({email});
+    const UserAlreadyExists = await User.findOne({email})
     if (UserAlreadyExists){
         throw new BadRequestError('Email already in use')
     }
     const user = await User.create({ name, email, password})
+
     const token = user.createJWT()
-    res
-        .status(StatusCodes.OK)
-        .json({ 
+    res.status(StatusCodes.CREATED).json({ 
             user:{
-                email:user.email, 
-                lastName:user.lastName, 
-                location:user.location, 
+                email: user.email, 
+                lastName: user.lastName, 
+                location: user.location, 
                 name: user.name
             }, 
-        token, 
-        loction: user.location })
+            token, 
+            loction: user.location, 
+        })
 }
 
 const login = async (req, res) => {
-    res.send('login user')
+    const { email, password } = req.body
+    if(!email || !password){
+        throw new BadRequestError('Please provide all values')
+    }
+    const user = await User.findOne({ email }).select('+password')
+    if(!user){
+        throw new UnauthenticatedError('Invalid Credentials')
+    }
+
+    const isPasswordCorrect = await user.comparePassword(password)
+    if(!isPasswordCorrect){
+        throw new UnauthenticatedError('Invalid Credentials')
+    }
+    const token = user.createJWT()
+    user.password = undefined
+    res.status(StatusCodes.OK).json({user, token, location: user.location })
 }
 
 const updateUser = async (req, res) => {
